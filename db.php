0dbb4d81634a9f733d0a80aa883f91f489c9fab1
TakaD-Dev <nmhao2205@gmail,com>
2026-03-29 17:58:28 +0000

    Update DB connection string

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "192.168.1.3"; // Thay bằng IP máy Ubuntu của ní
-$username = "root";          // User MySQL của ní
-$password = "123";           // Pass MySQL của ní
+$servername = "192.168.1.3"; // Địa chỉ IP máy Ubuntu của ní
+$username = "root";          // Tên đăng nhập MySQL
+$password = "123";           // Mật khẩu MySQL của ní
 $dbname = "project_qtm";     // Tên database ní đã tạo
 
-// Tạo kết nối
+// Khởi tạo kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Kiểm tra kết nối
+// Kiểm tra xem có thông không
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
