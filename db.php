81e6201e5dfb4e4a768f46c5c33284510f722957
TakaD-Dev <nmhao2205@gmail,com>
2026-03-31 20:12:16 +0000

    Fix: Final update with mysqli driver

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost"; // Hoặc IP của máy DB
-$user = "root";
-$pass = "";
+$host = "192.168.1.2"; // THAY BẰNG IP THẬT CỦA MÁY BẠN (Ví dụ 192.168.x.x)
+$user = "root";        // Đảm bảo User root này có quyền truy cập từ xa (Remote Access)
+$pass = "";            // Mật khẩu MySQL của bạn
 $dbname = "web_basic";
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
